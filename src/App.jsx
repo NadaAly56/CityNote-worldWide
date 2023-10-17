33b7d0a7478387d4a698edce142c8c9ec416aa0a
nada <nada.aly5613@gmail.com>
2023-10-17 11:35:30 +0300

    Setting up nested routing

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,21 @@ import PricingPage from "./pages/PricingPage";
 import ProductPage from "./pages/ProductPage";
 import Login from "./pages/LoginPage";
 import AppLayout from "./pages/AppLayout";
+import CityList from "./components/cityList/CityList";
 
 function App() {
 
   return <div>
     <BrowserRouter>
   <Routes>
-    <Route path="/" element={<Home/>}></Route>
+    <Route index element={<Home/>}></Route>
     <Route path="/app" element={<AppLayout/>}></Route>
     <Route path="/pricing" element={<PricingPage/>}></Route>
-    <Route path="/product" element={<ProductPage/>}></Route>
+    <Route path="/product" element={<ProductPage/>}>
+      <Route index path="cities" element={<CityList />}></Route>
+      <Route path="countries" element={<CityList />}></Route>
+      <Route path="form" ></Route>
+    </Route>
     <Route path="/login" element={<Login/>}></Route>
     <Route path="*" element={<NotFound/>}></Route>
   </Routes>
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import styles from './Sidebar.module.css'
 import Logo from '../logo/Logo'
+import AppNav from '../appNav/AppNav'
 export default function Sidebar() {
   return (
     <div className={styles.sidebar}>
         <Logo />
-       
+       <AppNav />
         <footer className={styles.footer}>
             <p className={styles.copyright}>
                 &copy; Copyright {new Date().getFullYear()} WorldWise Inc. 
